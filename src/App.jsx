837298e988ada0b66d7e5100b0135bffd6f77274
jawadali6688 <jawaddotprogrammer@gmail.com>
2024-04-02 05:26:05 +0500

    Mega Blog Project Using React with Redux and Redux Toolkit, Appwrite, Tailwind and some more libraries

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import config from "./config/config";
+
+
+function App() {
+
+console.log(config.appwriteProjectId);
+  return (
+<>
+<h1>A mega project using appwrite</h1>
+</>
+  )
+}
+
+export default App
