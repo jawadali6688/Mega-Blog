aa2b66a8d02774008ee216607c6871f68636090a
jawadali6688 <jawaddotprogrammer@gmail.com>
2024-04-18 23:33:11 +0500

    some code

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,29 @@
 import React, {useState} from 'react'
 import { login as authLogin } from '../store/authSlice'
+import { useDispatch } from 'react-redux'
+import { useForm } from 'react-hook-form'
 import {Link, useNavigate} from 'react-router-dom'
+import {Logo, Button, Input} from './index'
+import authService from '../appwrite/auth'
 function Login() {
+  const dispatch = useDispatch()
+  const navigate = useNavigate();
+  const {register, handleSubmit} = useForm()
+  const [error, setError] = useState("")
+
+  const login = async (data) => {
+    setError("");
+    try {
+      const session = await authService.login(data)
+      if (session) {
+        const userData = await authService.getCurrentUser();
+        if (userData) dispatch(authLogin(userData));
+        navigate("/")
+      }
+    } catch (error) {
+      setError(error)
+    }
+  }
   return (
     <div>
       
