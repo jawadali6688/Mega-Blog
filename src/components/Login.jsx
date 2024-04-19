82eac355c28325c33ff02ff937ac7df5e638f6df
jawadali6688 <jawaddotprogrammer@gmail.com>
2024-04-19 18:02:29 +0500

    some code

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -25,7 +25,11 @@ function Login() {
     }
   }
   return (
-    <div>
+    <div
+    className='flex items-center justify-center w-full'>
+      <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+
+      </div>
       
     </div>
   )
